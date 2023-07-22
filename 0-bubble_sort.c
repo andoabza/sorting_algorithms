ebf607d05a653a7cdbd228f07561826a66c518f0
ando <terbu102@gmail.com>
2023-07-21 22:57:12 -0700

    bubbly

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,12 +11,15 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned long int i;
 	unsigned long int j;
+	if (array == NULL || size < 2)
+		return;
 	for(i=0; i < size - 1; i++)
 	{
 		for(j=0; j < size - i - 1; j++)
 		{
 			if(array[j] > array[j+1]){
 				swap(&array[j], &array[j+1]);
+				print_array(array, size);
 			}
 		} 
 	}

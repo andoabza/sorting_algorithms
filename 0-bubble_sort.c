f36423f82f95411ced92a6fafc51a329cebd66fb
ando <terbu102@gmail.com>
2023-07-21 23:09:35 -0700

    bubbley dicription:

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,27 +6,37 @@
  *
  * Return : array by swaping two element
  */
-void swap(int* xp, int* yp);
+void swap(int *xp, int *yp);
+
 void bubble_sort(int *array, size_t size)
 {
 	unsigned long int i;
 	unsigned long int j;
 	if (array == NULL || size < 2)
 		return;
-	for(i=0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for(j=0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if(array[j] > array[j+1]){
-				swap(&array[j], &array[j+1]);
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
 			}
-		} 
+		}
 	}
-}	
-void swap(int* xp, int* yp)
-    {
-        int temp = *xp;
-        *xp = *yp;
-        *yp = temp;
-    }
+}
+/*
+ * swap - a functiontion that swaps the value.
+ * @*xp: first arg.
+ * @*yp: second arg
+ *
+ * Return - swapd values
+ */
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
